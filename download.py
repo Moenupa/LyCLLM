25a22007cf8f7a9b5de313b7d467113630757284
lwz <905328134@qq.com>
2026-03-19 13:45:22 +0800

    initial

--- download.py
+++ download.py
@@ -1,14 +1,16 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(
-    repo_id="Qwen/Qwen3-VL-2B-Instruct",
-    local_dir="../download/Qwen3-VL-2B-Instruct",
-    max_workers=16,
-)
+WORKERS = 8
 
+if __name__ == "__main__":
+    snapshot_download(
+        repo_id="Qwen/Qwen3-VL-2B-Instruct",
+        local_dir="../download/Qwen3-VL-2B-Instruct",
+        max_workers=WORKERS,
+    )
 
-snapshot_download(
-    repo_id="llava-hf/llava-onevision-qwen2-0.5b-ov-hf",
-    local_dir="../download/llava-onevision-qwen2-0.5b-ov-hf",
-    max_workers=16,
-)
+    snapshot_download(
+        repo_id="llava-hf/llava-onevision-qwen2-0.5b-ov-hf",
+        local_dir="../download/llava-onevision-qwen2-0.5b-ov-hf",
+        max_workers=WORKERS,
+    )
